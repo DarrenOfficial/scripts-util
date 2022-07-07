6a86ddf13bec29a0d2966af3c493fbf78a98c12c
Darren <github@darrennathanael.com>
2022-07-07 02:35:01 -0500

    default

--- general/realip_nginx.sh
+++ general/realip_nginx.sh
@@ -5,19 +5,19 @@ CURL_BIN=$(command -v curl)
 CF_IPV4=$($CURL_BIN -sL https://www.cloudflare.com/ips-v4)
 CF_IPV6=$($CURL_BIN -sL https://www.cloudflare.com/ips-v6)
 
-echo '' > /www/server/panel/vhost/nginx/cloudflare.conf
+echo '' > /etc/nginx/conf.d/cloudflare.conf
 echo "####################################"
 echo "Adding Cloudflare IPv4"
 echo "####################################"
 for cf_ip4 in $CF_IPV4; do
-    echo "set_real_ip_from $cf_ip4;" >> /www/server/panel/vhost/nginx/cloudflare.conf
+    echo "set_real_ip_from $cf_ip4;" >> /etc/nginx/conf.d/cloudflare.conf
 done
 echo "####################################"
 echo "Adding Cloudflare IPv6"
 echo "####################################"
 for cf_ip6 in $CF_IPV6; do
-    echo "set_real_ip_from $cf_ip6;" >> /www/server/panel/vhost/nginx/cloudflare.conf
+    echo "set_real_ip_from $cf_ip6;" >> /etc/nginx/conf.d/cloudflare.conf
 done
-echo 'real_ip_header CF-Connecting-IP;' >> /www/server/panel/vhost/nginx/cloudflare.conf
+echo 'real_ip_header CF-Connecting-IP;' >> /etc/nginx/conf.d/cloudflare.conf
 
 nginx -t && systemctl reload nginx
